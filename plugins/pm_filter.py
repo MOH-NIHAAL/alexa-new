b100d616c37ef23fbbfe95920829fe1f4a07ce19
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-05-22 00:55:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -765,5 +765,5 @@ async def auto_filter(client, message):
         elif imdb:
             await message.reply_text(f"**🏷 𝖳𝗂𝗍𝗅𝖾**: <a href={imdb['url']}>{imdb.get('title')}</a>\n**🎭 𝖦𝖾𝗇𝗋𝖾**: {imdb.get('genres')}\n**📆 𝖸𝖾𝖺𝗋**: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n**🌟 𝖱𝖺𝗍𝗂𝗇𝗀**: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10 </code>", reply_markup=InlineKeyboardMarkup(btn))
         else:
-            await message.reply_text(f"<b>𝖳𝗁𝖾𝗌𝖾 𝖺𝗋𝖾 𝖶𝗁𝖺𝗍 𝖨 𝖥𝗈𝗎𝗇𝖽 𝖥𝗈𝗋 {search} ‌‎ </b>", reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_text(f"<b>Hey 👋 {message.from_user.mention} 😍 \n\n 📁 Here is What I Found In My Database For Your Query : {search} 👇</b>", reply_markup=InlineKeyboardMarkup(btn))
 
