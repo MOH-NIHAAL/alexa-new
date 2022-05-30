d051e55a88766a2fe14d38ac83783f893ae4f2ce
Ꮴᴀᴍᴘɪʀᴇ <104682464+VAMPIRE0072@users.noreply.github.com>
2022-05-30 17:38:05 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
         ]],
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️</b>",
+            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\ncheck updates to know more.</b>",
             reply_markup=reply_markup)
     else:
         for u in message.new_chat_members:
